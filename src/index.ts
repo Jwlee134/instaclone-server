b0bcab1e0e6155bca39f12364a65b522d5361128
Jaewon <sorhd134@gmail.com>
2022-01-27 22:25:16 +0900

    Dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({ schema });
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
