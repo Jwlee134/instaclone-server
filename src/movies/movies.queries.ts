15c20878436ccc24592a2026508d48f3606765a8
Jaewon <sorhd134@gmail.com>
2022-01-31 14:45:12 +0900

    Create Account part One

--- src/movies/movies.queries.ts
+++
@@ -1,9 +0,0 @@
-import client from "../client";
-
-export default {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (root: any, { id }: any) =>
-      client.movie.findUnique({ where: { id } }),
-  },
-};
