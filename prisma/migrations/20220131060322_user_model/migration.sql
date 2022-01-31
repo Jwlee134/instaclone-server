a27cc1bf6d5ce9704348af15ad8d8dc7324939aa
Jaewon <sorhd134@gmail.com>
2022-01-31 15:22:17 +0900

    Create Account part Two

--- prisma/migrations/20220131060322_user_model/migration.sql
+++ prisma/migrations/20220131060322_user_model/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,3 +1,17 @@
+import client from "../client";
+
 export default {
-  Mutation: {},
+  Mutation: {
+    createAccount: async (
+      root: any,
+      { firstName, lastName, username, email, password }: any
+    ) => {
+      const user = await client.user.findFirst({
+        where: { OR: [{ username }, { email }] },
+      });
+      if (user) {
+        console.log("user exists");
+      }
+    },
+  },
 };
