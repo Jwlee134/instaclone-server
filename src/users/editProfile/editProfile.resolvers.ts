a9552f50cdff4be9971d80e8b0a9f66a8745dfa3
Jaewon <sorhd134@gmail.com>
2022-01-31 18:01:47 +0900

    editProfile

--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -1,8 +1,31 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
     editProfile: async (
       root: any,
       { firstName, lastName, username, email, password }: any
-    ) => {},
+    ) => {
+      let hashedPw = null;
+      if (password) {
+        hashedPw = await bcrypt.hash(password, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: { id: 1 },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(hashedPw && { password: hashedPw }),
+        },
+      });
+      if (updatedUser) {
+        return { isSuccess: true };
+      } else {
+        return { isSuccess: false, error: "Cannot update profile." };
+      }
+    },
   },
 };
