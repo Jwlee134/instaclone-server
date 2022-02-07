edcce5e0f41c4a2f498e66cb3c8d60fc3e246cd1
Jaewon <sorhd134@gmail.com>
2022-02-07 16:28:17 +0900

    authentication part One

--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -1,18 +1,23 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
     editProfile: async (
       root: any,
-      { firstName, lastName, username, email, password }: any
+      { firstName, lastName, username, email, password, token }: any
     ) => {
+      const id = jwt.verify(token, process.env.PRIVATE_KEY!);
+      if (!id) {
+        return { isSuccess: false, error: "Cannot update profile." };
+      }
       let hashedPw = null;
       if (password) {
         hashedPw = await bcrypt.hash(password, 10);
       }
       const updatedUser = await client.user.update({
-        where: { id: 1 },
+        where: { id: parseInt(id as string, 10) },
         data: {
           firstName,
           lastName,
