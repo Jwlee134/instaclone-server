5773765005d6b2103a41a86ccda8d38e79130229
Jaewon <sorhd134@gmail.com>
2022-02-09 17:00:03 +0900

    Morgan

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import schema from "./schema";
 import { getUser } from "./users/users.utils";
 import client from "./client";
 import { graphqlUploadExpress } from "graphql-upload";
+import logger from "morgan";
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -20,6 +21,7 @@ const startServer = async () => {
 
   const app = express();
 
+  app.use(logger("dev"));
   // process.cwd() => current working directory === /Users/jaewon/Desktop/instaclone-server
   // 브라우저 주소 표시줄에 http://localhost:4000/uploads/*에 접속하면 스태틱 파일들을 서빙한다는 의미
   app.use("/uploads", express.static(process.cwd() + "/uploads"));
