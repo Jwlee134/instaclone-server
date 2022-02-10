fa6aeab54bcda842bfd46ccffa567d981ebd126b
Jaewon <sorhd134@gmail.com>
2022-02-10 17:56:12 +0900

    Followers Pagination part Two

--- src/users/seeFollowers/seeFollowers.resolvers.ts
+++ src/users/seeFollowers/seeFollowers.resolvers.ts
@@ -3,10 +3,25 @@ import { Resolvers } from "../../types";
 const resolvers: Resolvers = {
   Query: {
     seeFollowers: async (root, { username, page }, { client }) => {
+      const targetUser = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!targetUser) {
+        return { isSuccess: false, error: "Cannot find user." };
+      }
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({ skip: (page - 1) * 5, take: 5 });
-      return { isSuccess: true, followers };
+      const totalFollowers = await client.user.count({
+        where: { following: { some: { username } } },
+      });
+      return {
+        isSuccess: true,
+        followers,
+        totalFollowers,
+        totalPages: Math.ceil(totalFollowers / 5),
+      };
     },
   },
 };
