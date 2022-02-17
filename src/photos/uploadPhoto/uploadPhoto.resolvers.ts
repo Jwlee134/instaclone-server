333c6d21434f97a382a68e67d74e61c4ee8a9126
Jaewon <sorhd134@gmail.com>
2022-02-17 17:16:32 +0900

    Upload Photo part Three

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -5,9 +5,23 @@ const resolvers: Resolvers = {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (root, { file, caption }, { loggedInUser, client }) => {
+        let hashtagObj;
         if (caption) {
-          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          const hashtags: string[] = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          hashtagObj = hashtags.map((tag) => ({
+            where: { hashtag: tag },
+            create: { hashtag: tag },
+          }));
         }
+        const photo = await client.photo.create({
+          data: {
+            file,
+            caption,
+            owner: { connect: { id: loggedInUser?.id } },
+            ...(hashtagObj && { hashtags: { connectOrCreate: hashtagObj } }),
+          },
+        });
+        return photo;
       }
     ),
   },
